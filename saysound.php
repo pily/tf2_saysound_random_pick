4e0226815670c99cc11538e5aa769fe182596c04
pily <piliang@nate.com>
2020-04-21 14:35:34 +0900

    신규 음악 우선 순위 추가

--- saysound.php
+++ saysound.php
@@ -6,7 +6,7 @@
         $pass = '';
         $db = 'saysound';
 
-        $conn = mysqli_connect($host, $user, $pass , $db);
+        $conn = mysqli_connect($host, $user, $pass, $db);
 
         if (!$conn) {
             //echo "Error: Unable to connect to MySQL." . PHP_EOL;
@@ -21,24 +21,75 @@
         return $conn;
     }
 
-    function get_saysound_list($type){
+    function get_saysound_old_list($type, $startpos, $count){
 
         $conn = connect_db();
 
         $stmt = $conn->prepare("SELECT date, number
                                 FROM ss_list
-                                WHERE type in(?, 'both')
+                                WHERE type IN(?, 'both')
                                 ORDER BY rand()
-                                LIMIT 30");
+                                LIMIT ?");
 
-        $stmt->bind_param('s', $type);
+        $stmt->bind_param('si', $type, $count);
+
+        $result = array();
+
+        if ($stmt) {
+            $stmt->execute();
+            $stmt->bind_result($date, $number);
+        
+            while($stmt->fetch()) {
+                if($startpos == 1){
+                    $index_string = "\""."file"."\"";
+                }else{
+                    $index_string = "\""."file".$startpos."\"";
+                }
+
+                if(strlen($number) == 1){
+                    $number = "00".$number;
+                }
+                else if(strlen($number) == 2)
+                {
+                    $number = "0".$number;
+                }
+
+                $path_string = "\""."ashita"."/"."320"."/".$date."/".$number.".mp3"."\"";
+
+                $string = $index_string." ".$path_string;
+
+                array_push($result, $string);
+                $startpos++;
+                
+            }
+            $stmt->close();
+        }
+        
+        $conn->close();
+
+        return $result;
+
+    }
+
+    function get_saysound_new_list($type){
+
+        $conn = connect_db();
+
+        $stmt = $conn->prepare("SELECT date, number
+                                FROM ss_list
+                                WHERE type IN(?, 'both') AND date = ?
+                                ORDER BY rand()");
+
+        $maxdate = get_maxdate();
+
+        $stmt->bind_param('si', $type, $maxdate);
 
         $result = array();
         $index = 1;
 
         if ($stmt) {
             $stmt->execute();
-            $stmt->bind_result($date,$number);
+            $stmt->bind_result($date, $number);
         
             while($stmt->fetch()) {
                 if($index == 1){
@@ -46,12 +97,20 @@
                 }else{
                     $index_string = "\""."file".$index."\"";
                 }
-               
+
+                if(strlen($number) == 1){
+                    $number = "00".$number;
+                }
+                else if(strlen($number) == 2)
+                {
+                    $number = "0".$number;
+                }
+
                 $path_string = "\""."ashita"."/"."320"."/".$date."/".$number.".mp3"."\"";
 
                 $string = $index_string." ".$path_string;
 
-                array_push($result,$string);
+                array_push($result, $string);
                 $index++;
                 
             }
@@ -64,6 +123,76 @@
 
     }
 
-    var_dump(get_saysound_list('win'));
+    function get_maxdate(){
+
+        $conn = connect_db();
+        $stmt = $conn->prepare("SELECT MAX(date)
+                                FROM ss_list
+                                WHERE 1=1");
+
+        if ($stmt) {
+            $stmt->execute();
+            $stmt->bind_result($maxdate);
+            while($stmt->fetch()) {
+               $result = $maxdate;
+            }
+            $stmt->close();
+        }
+        
+        $conn->close();
+
+        return $result;
+
+    }
+
+    function get_new_count($type){
+
+        $conn = connect_db();
+
+        $maxdate = get_maxdate();
+
+        $stmt = $conn->prepare("SELECT count(date)
+                                FROM ss_list
+                                WHERE type IN(?, 'both') AND date = ?");
+
+        $stmt->bind_param('si', $type, $maxdate);
+
+        if ($stmt) {
+            $stmt->execute();
+            $stmt->bind_result($count);
+            while($stmt->fetch()) {
+               $result = $count;
+            }
+            $stmt->close();
+        }
+        
+        $conn->close();
+
+        return $result;
+
+    }
+
+    //echo var_dump(get_maxdate());
+    $song = 40;
+
+    $new_win_song = get_new_count('win');
+    $new_lose_song = get_new_count('lose');
+    
+    $left_win_song = $song - $new_win_song;
+    $left_lose_song = $song - $new_lose_song;
+    
+    $new_win_list = get_saysound_new_list('win');
+    $old_win_list = get_saysound_old_list('win', $new_win_song + 1, $left_win_song);
+
+    $win_list = array_merge($new_win_list,$old_win_list);
+
+    var_dump($win_list);
+
+    $new_lose_list = get_saysound_new_list('lose');
+    $old_lose_list = get_saysound_old_list('lose', $new_lose_song + 1, $left_lose_song);
+
+    $lose_list = array_merge($new_lose_list,$old_lose_list);
+
+    var_dump($lose_list);
 
 ?>
